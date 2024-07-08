11086f220726c6abf74342e240e3547f673bd7a9
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2024-07-08 20:09:44 +0300

    clean: running linting command.

--- server/src/utils/validators/authValidator.ts
+++ server/src/utils/validators/authValidator.ts
@@ -1,11 +1,7 @@
 import { body } from 'express-validator';
 import { validator } from './../../middleware';
 
-export const loginValidator = [
-  body('email').isEmail(),
-  body('password').isLength({ min: 8 }),
-  validator,
-];
+export const loginValidator = [body('email').isEmail(), body('password').isLength({ min: 8 }), validator];
 
 export const signupValidator = [
   body('email').isEmail(),
